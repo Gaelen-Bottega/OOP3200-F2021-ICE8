45441fc535ab5e51edac8ae04e5c56668c006016
Gaelen <gaelenrhoads@gmail.com>
2021-11-10 15:53:57 -0500

    Basic output and input

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,28 @@
 package ca.durhamcollege;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        // Input declarations
+        String name = null;
+        float age = 0.0f;
+
+        // Output to user statements
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+
+        System.out.print("Please enter your age: ");
+        age = keyboard.nextFloat();
+
+        // Print results
+        System.out.printf("%s\n", name);
+        System.out.printf("%.1f", age);
+
+
     }
 }
